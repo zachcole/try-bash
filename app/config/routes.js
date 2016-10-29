66b207ff0bd398ff3e447790737557ded6c2aa70
Zach Cole <zach.cole@getbellhops.com>
2016-10-29 13:50:04 -0400

    changing route history from hash to browser

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,12 +3,12 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var hashHistory = ReactRouter.hashHistory;
+var browserHistory = ReactRouter.browserHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 		<Route path='/' component={Main}>
 			<IndexRoute component={Home} />
 		</Route>
